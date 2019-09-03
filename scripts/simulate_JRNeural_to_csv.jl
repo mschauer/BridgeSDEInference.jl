44adf1fd183498d00dd71efc85bc1c4e0a974717
SebaGraz <sebastiano.grazzi@gmail.com>
2019-09-03 16:03:32 +0200

    change values for the simulation

--- scripts/simulate_JRNeural_to_csv.jl
+++ scripts/simulate_JRNeural_to_csv.jl
@@ -15,22 +15,32 @@ FILENAME_OUT = joinpath(OUT_DIR,
 
 ### parameters as Table 1  of
 # https://mathematical-neuroscience.springeropen.com/articles/10.1186/s13408-017-0046-4
-P = JRNeuralDiffusion(3.25, 0.1, 22.0, 0.05 , 135.0, 5.0, 6.0, 0.56, 0.0, 220.0, 0.0, 0.01 , 2000.0, 1.0)
+P = JRNeuralDiffusion(3.25, 100.0, 22.0, 50.0 , 135.0, 5.0, 6.0, 0.56, 0.0, 220.0, 0.0, 0.01 , 2000.0, 1.0)
 # starting point under :regular parametrisation
-x0 = ℝ{6}(0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
 
-dt = 1/50000
-T = 200.0
+display(P)
+
+
+x0 = ℝ{6}(0.08,18,15,-0.5,0,0)
+
+dt = 1/100000
+T = 20.0
 tt = 0.0:dt:T
 
 Random.seed!(4)
 XX, _ = simulateSegment(ℝ{3}(0.0, 0.0, 0.0), x0, P, tt)
-#weird values
+
+XX
+
 
 XX.yy
-num_obs = 100
+num_obs = 10000
 skip = div(length(tt), num_obs)
 Time = collect(tt)[1:skip:end]
 #observation x[2]- x[3]
 df = DataFrame(time=Time, x1=[x[2] - x[3] for x in XX.yy[1:skip:end]])
 CSV.write(FILENAME_OUT, df)
+
+
+
+plot(df.time, df.x1)
